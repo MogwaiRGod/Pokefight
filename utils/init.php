c1a3c96cb2a68209f1dfc63f56b0e05e2aa0c2ab
Mogwai <diane.roberg@gmail.com>
2023-04-10 11:20:16 +0200

    utils

---
+++ utils/init.php
@@ -0,0 +1,7 @@
+<?php
+
+// import de toutes les fonctionnalités nécessaires au démarrage du site et à son bon fonctionnement
+require_once 'error_handler.php';
+require_once 'utils.php';
+require_once 'autoload.php';
+require_once 'router.php';
---
+++ utils/router.php
@@ -0,0 +1,61 @@
+<?php
+
+// à cause du Rewriting de l'URL, il y a par défaut un paramètre (vide) dans l'URL => $_GET a un élément
+// s'il y a des paramètres dans l'URL
+
+// récupération des composants de l'URL
+$params = explode(
+    '/',
+    /* On a défini dans le .htaccess que, dans l'URL, les paramètres seraient nommés p ;
+    on récupère donc dans $_GET (car c'est avec la méthode GET que les paramètres apparaissent dans l'URL)
+    la VALEUR des paramètres */
+    $_GET['p']
+);
+
+/**
+ * param[0] : controller
+ * param[1] : méthode
+ * param[2]++ : arguments
+ */ 
+
+// on évalue les paramètres extraits de l'URL pour définir la route demandée
+// s'il y a un paramètre dans l'URL -> on définit le controller correspondant
+if ($params[0]) {
+    $controller = $params[0];
+
+} else {
+    $controller = 'Home';
+}
+if (isset($params[1])) {
+    $method = $params[1];
+} else {
+    $method = 'Index';
+}
+if (isset($params[2])) {
+    $req = $params[2];
+} else {
+    $req = '';
+}
+
+// $called est le chemin de la classe à appeler
+$called = 'controllers/' . $controller . '.php';
+// on importe le controller correspondant
+require ($called);
+
+// si une méthode a été entrée dans l'URL => 
+// vérification qu'elle existe
+if (method_exists(/* classe */ $controller,/* méthode à vérifier */ $method)){
+    // instanciation du controller appelé dans l'URL
+    $newController = new $controller();
+    // invocation de sa méthode appelée dans l'URL
+    if (isset($args)) {
+        // si des arguments on été entrés dans l'URL
+        $newController->$method($args);
+    }
+    else {
+        $newController->$method();
+    }
+}
+else {
+    echo 'Erreur 404 : méthode non-existante';
+}
